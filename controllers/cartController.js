9c6700884033d9e9a171ff3d13a27ee6076f3f2f
VincentAitzz <150597557+VincentAitzz@users.noreply.github.com>
2024-07-05 14:43:53 -0400

    Actualizacion

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -50,57 +50,72 @@ exports.getCart = (req, res) => {
     }
 };
 
-
 exports.addToCart = (req, res) => {
     if (req.session.loggedin) {
-        const userId = req.session.userId;
-        const productId = req.body.productID;
-        const quantity = req.body.quantity || 1;
-        const cartId = req.session.cartId;
-
-        // Verificar si el producto ya está en el carrito
-        connection.query('SELECT * FROM carrito_productos WHERE carrito_id = ? AND producto_id = ?', [cartId, productId], (error, cartProduct) => {
+      const userId = req.session.userId;
+      const productId = req.body.productId; // Usar el nombre correcto del parámetro
+      const quantity = req.body.quantity || 1;
+      const cartId = req.session.cartId;
+  
+      console.log('Producto: '+ productId)
+      // Verificar si el producto existe en la tabla productos
+      connection.query('SELECT id FROM productos WHERE id = ?', [productId], (error, result) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send('Error al agregar al carrito');
+        } else if (result.length === 0) {
+          // El producto no existe en la tabla productos
+          res.status(400).send('El producto no existe');
+        } else {
+          // Verificar si el producto ya está en el carrito
+          connection.query('SELECT * FROM carrito_productos WHERE carrito_id = ? AND producto_id = ?', [cartId, productId], (error, cartProduct) => {
             if (error) {
-                console.error(error);
-                res.status(500).send('Error al agregar al carrito');
+              console.error(error);
+              res.status(500).send('Error al agregar al carrito');
             } else if (cartProduct.length > 0) {
-                // Actualizar la cantidad del producto en el carrito
-                connection.query('UPDATE carrito_productos SET cantidad = ? WHERE carrito_id = ? AND producto_id = ?', [cartProduct[0].cantidad + quantity, cartId, productId], (error, result) => {
-                    if (error) {
-                        console.error(error);
-                        res.status(500).send('Error al agregar al carrito');
-                    } else {
-                        res.redirect('back');
-                    }
-                });
+              // Actualizar la cantidad del producto en el carrito
+              connection.query('UPDATE carrito_productos SET cantidad = cantidad + ? WHERE carrito_id = ? AND producto_id = ?', [quantity, cartId, productId], (error, result) => {
+                if (error) {
+                  console.error(error);
+                  res.status(500).send('Error al agregar al carrito');
+                } else {
+                  res.redirect('back');
+                }
+              });
             } else {
-                // Agregar el producto al carrito
-                connection.query('INSERT INTO carrito_productos (carrito_id, producto_id, cantidad) VALUES (?, ?, ?)', [cartId, productId, quantity], (error, result) => {
-                    if (error) {
-                        console.error(error);
-                        res.status(500).send('Error al agregar al carrito');
-                    } else {
-                        res.redirect('back');
-                    }
-                });
+              // Agregar el producto al carrito
+              connection.query('INSERT INTO carrito_productos (carrito_id, producto_id, cantidad) VALUES (?, ?, ?)', [cartId, productId, quantity], (error, result) => {
+                if (error) {
+                  console.error(error);
+                  res.status(500).send('Error al agregar al carrito');
+                } else {
+                  res.redirect('back');
+                }
+              });
             }
-        });
+          });
+        }
+      });
     } else {
-        res.render('login', {
-            companyName: 'Cloup_co',
-            companyDescription: 'Somos una empresa dedicada a la venta de productos en línea.',
-            name: '',
-            login: false
-        });
+      res.render('login', {
+        companyName: 'Cloup_co',
+        companyDescription: 'Somos una empresa dedicada a la venta de productos en línea.',
+        name: '',
+        login: false
+      });
     }
-};
+  };
 
 
 exports.removeFromCart = (req, res) => {
     if (req.session.loggedin) {
         const productId = req.body.productID;
+        const cartId = req.session.cartId;
 
-        connection.query('DELETE FROM carrito WHERE usuario_id = ? AND producto_id = ?', [req.session.userID, productId], (error, result) => {
+        console.log('idProducto: '+productId);
+        console.log('idCarrito: '+cartId);
+
+        connection.query('DELETE FROM carrito_productos WHERE carrito_id = ? AND producto_id = ?', [cartId, productId], (error, result) => {
             if (error) {
                 console.error(error);
                 res.status(500).send('Error al eliminar del carrito');
